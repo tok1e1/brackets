c9e39f043e831259fe191b0e70a796fb8a0fc13e
tok1e1 <dan.18.sh@yandex.ru>
2024-08-12 19:20:29 +0400

    fix: brackets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   const stack = [];
-  let symbols = new Map(bracketsConfig);
-  for (const s of str){
-    if (symbols.has(s))
-      stack.push(s);
-    else{
+  const symbols = new Map(bracketsConfig);
+  const openingBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  const closingBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+
+  for (const s of str) {
+    if (openingBrackets.has(s)) {
+      if (stack.length > 0 && stack[stack.length - 1] === s && closingBrackets.has(s)) {
+        stack.pop();
+      } else {
+        stack.push(s);
+      }
+    } else {
       const lastS = stack.pop();
-      if(!lastS || symbols.get(lastS) !== s){
+      if (!lastS || symbols.get(lastS) !== s) {
         return false;
       }
     }

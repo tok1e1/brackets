9d1c243b6dba1c2265dd3ac8cb911900a9a6886b
tok1e1 <dan.18.sh@yandex.ru>
2024-08-12 19:17:58 +0400

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  let symbols = new Map(bracketsConfig);
+  for (const s of str){
+    if (symbols.has(s))
+      stack.push(s);
+    else{
+      const lastS = stack.pop();
+      if(!lastS || symbols.get(lastS) !== s){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
